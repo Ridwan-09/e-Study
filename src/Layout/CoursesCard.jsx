694de1d7ca509e3ccfd1263ee3918e864ade6d52
Ridwan Salim <ridwan.salim@gmail.com>
2024-02-08 19:48:17 +0600

    first commit

--- src/Layout/CoursesCard.jsx
+++ src/Layout/CoursesCard.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const CoursesCard = (props) => {
     return (
-        <div>
-            <div>
-                <img src="" alt="" />
+        <div className='flex flex-col items-center justify-between bg-white border-2 border-LightText md:border-none md:w-2/5 p-5 cursor-pointer rounded-lg hover:shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] transition-all'>
+            <div className='w-3/5'>
+                <img src={props.img} alt="img" />
             </div>
             <div>
                 <h3>{props.title}</h3>
