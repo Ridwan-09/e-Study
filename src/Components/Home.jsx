dfd1aa2a4dd370b2fec8cc45dc8bbe68ce1628de
Ridwan Salim <ridwan.salim@gmail.com>
2024-02-08 17:48:29 +0600

    first commit

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
-import { Button} from 'react-scroll';
-import { Link } from 'react-router-dom';
+import { Link } from 'react-scroll';
+import Button from '../Layout/Button';
 
 const Home = () => {
     return (
         <div>
             <div>
-                <h2>Knowledge With</h2><span>eStudy</span>
+                <h2>Knowledge With</h2>
+                <span>eStudy</span>
 
-                <p>eStudy is your gateway to a world of limitless learning possibilities. With our cutting-edge eLearning platform, you can explore a vast library of courses, from academic subjects to practical skills, all designed to help you achieve your goals</p>
+                <p>
+                    eStudy is your gateway to a world of limitless learning possibilities. With our cutting-edge eLearning platform, you can explore a vast library of courses, from academic subjects to practical skills, all designed to help you achieve your goals
+                </p>
 
                 <Link to="contact" spy={true} smooth={true} duration={500}>
-                <Button></Button>
+                    <Button></Button>
                 </Link>
             </div>
         </div>
