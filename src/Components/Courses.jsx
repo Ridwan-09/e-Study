c8db6fbbc8abd1a6d876242c31a9663ac8e1da98
Ridwan Salim <ridwan.salim@gmail.com>
2024-02-08 19:40:49 +0600

    first commit

--- src/Components/Courses.jsx
+++ src/Components/Courses.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import Heading from '../Layout/Heading';
+import webImg from '../assets/web-dev.svg';
+import appImg from '../assets/App-dev.svg';
+import graphicImg from '../assets/graphic.svg';
+import digitalImg from '../assets/digital.svg';
+import CoursesCard from '../Layout/CoursesCard';
 
 const Courses = () => {
     return (
         <div className='min-h-screen flex flex-col items-center md:px-32 px-5 my-10'>
             <Heading title1='Our' title2='Courses'></Heading>
-            
+
+            <div>
+                <CoursesCard img={webImg} title='Web Development'></CoursesCard>
+                <CoursesCard img={appImg} title='App Development'></CoursesCard>
+                <CoursesCard img={graphicImg} title='Graphic Designing'></CoursesCard>
+                <CoursesCard img={digitalImg} title='Digital Marketing'></CoursesCard>
+            </div>
+
         </div>
     );
 };
