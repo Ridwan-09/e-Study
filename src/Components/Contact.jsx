5b41777cfb3f81bd73ee8ab4ac0855081bbcddde
Ridwan Salim <ridwan.salim@gmail.com>
2024-02-08 20:38:25 +0600

    first commit

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -9,8 +9,8 @@ const Contact = () => {
             <Heading title1='Contact' title2='Us'></Heading>
 
             <div className='flex flex-col md:flex-row justify-between w-full'>
-                <form>
-                    <div>
+                <form className='w-full md:w-2/5 space-y-5 pt-20'>
+                    <div className='flex flex-col'>
                         <label htmlFor="userName">Your Name</label>
                         <input
                             className='py-3 px-2 rounded-lg hover:shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] transition-all'
@@ -19,7 +19,8 @@ const Contact = () => {
                             id='userName'
                             placeholder='enter your name' />
                     </div>
-                    <div>
+
+                    <div className='flex flex-col'>
                         <label htmlFor="userEmail">Your Email</label>
                         <input
                             className='py-3 px-2 rounded-lg hover:shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] transition-all'
@@ -28,7 +29,8 @@ const Contact = () => {
                             id='userEmail'
                             placeholder='enter your email' />
                     </div>
-                    <div>
+
+                    <div className='flex flex-col'>
                         <label htmlFor="userNumber">Your Number</label>
                         <input
                             className='py-3 px-2 rounded-lg hover:shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] transition-all'
@@ -43,7 +45,7 @@ const Contact = () => {
                     </div>
                 </form>
 
-                <div>
+                <div className='w-full md:w-2/4'>
                     <img src={img} alt="img" />
                 </div>
             </div>
