3e36bc2028174528e768f759a1860f7c60342fdc
Ridwan Salim <ridwan.salim@gmail.com>
2024-02-08 15:33:44 +0600

    first commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'react-scroll';
 
 const Navbar = () => {
     return (
         <div>
             <div>
                 <div>
-                    
+                    <Link>
+                        
+                    </Link>
                 </div>
                 <div>
 
