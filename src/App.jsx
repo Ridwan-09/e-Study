8f0e465cf134ee463754e95c13efecba4669099c
Ridwan Salim <ridwan.salim@gmail.com>
2024-02-08 17:44:21 +0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import Navbar from './Components/Navbar';
+import Home from './Components/Home';
 
 const App = () => {
   return (
     <div>
       <Navbar></Navbar>
+
+      <main>
+        <div id='home'>
+          <Home></Home>
+        </div>
+      </main>
     </div>
   );
 };
